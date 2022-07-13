51a912cab197409948029bb93b0c03040ca72e0b
Christian Cecilia <christian.cecilia1@gmail.com>
2022-07-13 09:27:11 -0400

    WIP, only started

--- src/sw.ts
+++ src/sw.ts
@@ -1,1 +1,15 @@
-export const test = false;
+/// <reference lib="WebWorker" />
+
+// export empty type because of tsc --isolatedModules flag
+export type {};
+// eslint-disable-next-line no-undef
+declare const self: ServiceWorkerGlobalScope;
+
+self.addEventListener('install', function (event) {
+  console.log('ServiceWorker | isntalled ', event);
+});
+
+self.addEventListener('install', function (event) {
+  console.log('ServiceWorker | isntalled ', event);
+  return self.clients.claim();
+});
